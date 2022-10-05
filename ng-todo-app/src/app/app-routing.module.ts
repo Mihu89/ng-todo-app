f3af08ae5a16adae295ed2925902940a87eeee99
Mihail <mcoscodan@ranorex.com>
2022-10-05 19:56:20 +0300

    Add todos component, fix navigation

--- ng-todo-app/src/app/app-routing.module.ts
+++ ng-todo-app/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { AboutComponent } from './components/pages/about/about.component';
-import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodosComponent } from './components/todos/todos.component';
 
 const routes: Routes = [
-  // { path: '', component:AppComponent},
+  { path: '', component:TodosComponent},
   {path: 'about', component: AboutComponent}
 ];
 
--- ng-todo-app/src/app/app.module.ts
+++ ng-todo-app/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { AboutComponent } from './components/pages/about/about.component';
+import { TodosComponent } from './components/todos/todos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AboutComponent
+    AboutComponent,
+    TodosComponent
   ],
   imports: [
     BrowserModule,
